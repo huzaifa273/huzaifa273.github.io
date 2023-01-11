dfe36b3a8e5d457cdcea3369a369b9f6bbbd5cfe
huzaifa273 <malikhuzaifaawais1@gmail.com>
2023-01-11 20:45:31 +0500

    portfolio

---
+++ portfolio/app.js
@@ -0,0 +1,10 @@
+var navToggle = document.querySelector(".nav-toggle");
+var links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function() {
+    links.classList.toggle("show-links");
+});
+
+links.addEventListener("click", function() {
+    links.classList.remove("show-links");
+});
