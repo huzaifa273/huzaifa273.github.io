65f9d0d5699901bec5ced15680b7f2203b00cfee
huzaifa273 <malikhuzaifaawais1@gmail.com>
2023-06-13 09:59:37 +0500

    Travelo updated

--- travelo/js/app.js
+++ travelo/js/app.js
@@ -1,3 +1,4 @@
+
 const parallax_el = document.querySelectorAll(".parallax")
 
 let xValue = 0, yValue = 0;
@@ -19,13 +20,35 @@ const registration = document.getElementById('registration');
 
 hamburgerBtn.addEventListener('click', () => {
     registration.classList.toggle('show')
-    // if(registration.classList.contains('show')){
-    //     window.addEventListener('click', () => {
-    //         if(registration.classList.contains('show')){
-    //             registration.classList.remove('show')
-    //             console.log("hello");
-    //         }else{
-    //         };
-    //     })
-    // };
 });
+
+
+// Eye tracker
+
+$("body").mousemove(function(event) {
+    var eye = $(".eye");
+    var x = (eye.offset().left) + (eye.width() / 2);
+    var y = (eye.offset().top) + (eye.height() / 2);
+    var rad = Math.atan2(event.pageX - x, event.pageY - y);
+    var rot = (rad * (180 / Math.PI) * -1) + 180;
+    eye.css({
+      '-webkit-transform': 'rotate(' + rot + 'deg)',
+      '-moz-transform': 'rotate(' + rot + 'deg)',
+      '-ms-transform': 'rotate(' + rot + 'deg)',
+      'transform': 'rotate(' + rot + 'deg)'
+    });
+});
+
+
+// Changing the color of the orbit text 
+const color = document.getElementById('color');
+window.onscroll = function () { 
+    if (document.body.scrollTop >= 800 || document.documentElement.scrollTop >= 800 ) {
+        color.classList.remove("color");
+        color.classList.add("dark-color");
+    } 
+    else {
+        color.classList.remove("dark-color");
+        color.classList.add("color");
+    }
+};
